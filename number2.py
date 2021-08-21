1a7b9139a7401edb9944324efc5f38f46fc703b3
seyed ali mirzaee zohan <88889881+seyed-ali-mirzaee-zohan@users.noreply.github.com>
2021-08-21 13:41:31 +0430

    Update number2.py

--- number2.py
+++ number2.py
@@ -1,9 +1,17 @@
-print('a*x**2+b*x+c=0')
+from math import sqrt
 def Quadratic_equation():
-    a=float(input('Please enter the value of a :'))
-    b=float(input('Please enter the value of b :'))
-    c=float(input('Please enter the value of c :'))
-    x1=float(-b+(b**2-4*a*c)**0.5)/(2*a)
-    x2=float(-b-(b**2-4*a*c)**0.5)/(2*a)
-    print('x1:',x1,', x2:',x2)
+    print('a*x**2+b*x+c=0')
+    a=int(input('Please enter the value of a :'))
+    b=int(input('Please enter the value of b :'))
+    c=int(input('Please enter the value of c :'))
+    delta=(b**2-4*a*c)
+    if delta>0 :    
+        x1=(-b+sqrt(delta))/(2*a)
+        x2=(-b-sqrt(delta))/(2*a)
+        print('x1:',x1,', x2:',x2)
+    if delta==0:
+        x1=(-b)/(2*a)
+        print('x1:',x1)
+    if delta<0:
+        print("The equation has no answer !! ")
 Quadratic_equation()
